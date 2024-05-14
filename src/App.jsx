f254c2b4d5274363327fac48df48701e0fe6def3
brunothuroba <lima25759@gmail.com>
2024-05-14 14:03:18 -0300

    git para o incode

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+       author="Bruno Emanuel"
+       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ipsa. Quasi veritatis sed soluta minus quibusdam ratione quae, dolorum assumenda fugiat tenetur, impedit atque beatae alias, quidem odio officia necessitatibus."
+      />
+      <Post 
+        author= "Kelly"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }

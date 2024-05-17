b31e8b5775aff18aea132e7647d69f9929dc3563
brunoemanuel21 <lima2575@outlook.com>
2024-05-17 17:41:11 -0300

    commit feito no estagio dia 17/05

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { Post } from './Post';
+import { Header } from './components/Header';
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
        author="Bruno Emanuel"
        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ipsa. Quasi veritatis sed soluta minus quibusdam ratione quae, dolorum assumenda fugiat tenetur, impedit atque beatae alias, quidem odio officia necessitatibus."

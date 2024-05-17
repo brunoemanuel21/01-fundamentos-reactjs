d452e8681e6f6bb7417d6cbf63e15e44c292b8d1
brunothuroba <lima25759@gmail.com>
2024-05-17 00:44:28 -0300

    commite de reverter, pois teve problemas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Post } from './Post';
-import { Header } from './components/Header';
-import './global.css'; 
-
-import styles from './App.modules.css';
 
 export function App() {
   return (
     <div>
-      <Header />
-
-      <div className={styles}></div>
+      <Post
+       author="Bruno Emanuel"
+       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ipsa. Quasi veritatis sed soluta minus quibusdam ratione quae, dolorum assumenda fugiat tenetur, impedit atque beatae alias, quidem odio officia necessitatibus."
+      />
+      <Post 
+        author= "Kelly"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }

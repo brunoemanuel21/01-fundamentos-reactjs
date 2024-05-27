319dc61d6db26ac0c0d086c8479bade786cb309f
brunoemanuel21 <lima2575@outlook.com>
2024-05-27 17:15:31 -0300

    git para casa

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post
-       author="Bruno Emanuel"
-       content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ipsa. Quasi veritatis sed soluta minus quibusdam ratione quae, dolorum assumenda fugiat tenetur, impedit atque beatae alias, quidem odio officia necessitatibus."
-      />
-      <Post 
-        author= "Kelly"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Bruno Emanuel"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ipsa. Quasi veritatis sed soluta minus quibusdam ratione quae, dolorum assumenda fugiat tenetur, impedit atque beatae alias, quidem odio officia necessitatibus."
+          />
+          <Post
+            author="Kelly"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
     </div>
   )
 }
